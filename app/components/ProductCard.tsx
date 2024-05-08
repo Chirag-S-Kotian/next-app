bab2d3d04038fa9fbb0d39be091a0719c5ce049b
Chirag-S-kotian <ckotian117@gmail.com>
2024-05-08 19:33:51 +0530

    "Replace 'Add to cart' button with `AddToCart` component"
    Explanation:
    The code diff shows that a new component `AddToCart` is being imported and used in place of the existing 'Add to cart

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,10 +1,11 @@
 "use client";
 import React from "react";
+import AddToCart from "./AddToCart";
 
 const ProductCard = () => {
   return (
     <div>
-      <button onClick={() => console.log("click")}>Add to cart</button>
+      <AddToCart />
     </div>
   );
 };
