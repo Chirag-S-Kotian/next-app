f444bb4f68d62044c9582524ae249c0323ec2367
Chirag-S-kotian <ckotian117@gmail.com>
2024-05-08 20:09:50 +0530

    Remove module class from ProductCard and use inline styles
    Explanation:
    This code diff shows the removal of the imported module class styles in the ProductCard component and the use of inline styles instead.

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,11 +1,10 @@
 "use client";
 import React from "react";
 import AddToCart from "./AddToCart";
-import styles from "./ProductCard.module.css"
 
 const ProductCard = () => {
   return (
-    <div className={styles.card}>
+    <div className="p-5 my-5 bg-sky-400 text-white text-4xl hover:bg-sky-600">
       <AddToCart />
     </div>
   );
